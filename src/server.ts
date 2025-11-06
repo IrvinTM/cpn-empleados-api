ed1011947c4a4076eb4cb214312a1ea81cc311f1
IrvinTM <irvintm23@gmail.com>
2025-11-05 20:33:21 -0600

    first

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from "express";
+import "dotenv/config"
+
+const app: Application = express();
+const PORT: number = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+const tesEnv = process.env.NODE_ENV
+
+console.log(tesEnv)
+
